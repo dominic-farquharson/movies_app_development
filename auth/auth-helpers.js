aa20c06ddf696f0c75e7dfcced9b23da21cf8721
Franklin Brooks <franklinchristopherbrooks@gmail.com>
2017-01-31 10:36:21 -0500

    add user profile page route, loginRequired helper method

--- auth/auth-helpers.js
+++ auth/auth-helpers.js
@@ -5,3 +5,30 @@ const models = require('../db/models/index');
 function comparePass(userPassword, databasePassword) {
   return bcrypt.compareSync(userPassword, databasePassword);
 }
+
+function createUser(req, res) {
+  const salt = bcrypt.genSaltSync();
+  const hash = bcrypt.hashSync(req.body.password, salt);
+
+  return models.User.create({
+    username: req.body.username,
+    password: hash,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    dob: req.body.dob
+  });
+}
+
+function loginRequired(req, res, next) {
+  if (!req.user) res.redirect('/auth/login');
+
+  return next();
+}
+
+module.exports = {
+  comparePass,
+  loginRedirect,
+  loginRequired,
+  createUser
+}
